29bb9764c3f6ecab89d79d2745c12929ebeb2ef5
Blake Chambers <blakejwc@alum.mit.edu>
2026-08-06 19:56:12 -0400

    Fixing list children indent and checkboxes.

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
+const markdownItTaskLists = require("markdown-it-task-lists");
+
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = function(eleventyConfig) {
 
     // Copy the `assets` directory to `_site`
     eleventyConfig.addPassthroughCopy("css");
+
+    eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(markdownItTaskLists));
 
     return {
         dir: {
